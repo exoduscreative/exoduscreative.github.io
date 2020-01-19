b5db72c95a5aeb01a36a5308ab1c4712d58d0dc9
exoduscreative <59782083+exoduscreative@users.noreply.github.com>
2020-01-18 20:20:40 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,8 +1,60 @@
 $(document).ready(function() {
 
+	// REDIRECT TO HOME
 	$('#icon').click(function() {
 		window.location.href = "index.html";
-		console.log('hi')
+	});
+
+
+	// LEFT AND RIGHT SCROLL BUTTONS (DESKTOP)
+	$('#right-arrow').click(function() {
+		$('#scrolling-wrapper').animate({
+			scrollLeft: '+=600px'
+		}, 'slow')
+	});
+	$('#left-arrow').click(function() {
+		$('#scrolling-wrapper').animate({
+			scrollLeft: '-=600px'
+		}, 'slow')
+	});
+
+
+	// OPEN MODAL IMAGE (DESKTOP)
+	$('.image').click(function() {
+		if ($(window).width() > 750) {
+			$('#modal').attr('style', 'display: block');
+			$('#modal-img').attr('src', $(this).find('img').attr('src'));
+			$('#close').click(function() {
+				$('#modal').attr('style', 'display: none');
+			});
+			$('#modal').click(function() {
+				$('#modal').attr('style', 'display: none');
+			});
+		};
+	});
+
+
+	// SCROLL TO TOP (MOBILE)
+	$('#mobile-logo').click(function() {
+  		$('#scrolling-wrapper').animate({ scrollTop: 0 }, 'slow');
+  		return false;
+	})
+
+
+	// OPEN MOBILE MENU
+	$('#hamburger').click(function() {
+		$('#mobile-menu').attr('style', 'display: block');
+		$('#hamburger').attr('style', 'display: none');
+		$('#exit-menu').attr('style', 'display: block');
+		$('#mobile-menu').attr('class', 'animated slideInDown');
+	});
+
+
+	// CLOSE MOBILE MENU
+	$('#exit-menu').click(function() {
+		$('#hamburger').attr('style', 'display: block');
+		$('#exit-menu').attr('style', 'display: none');
+		$('#mobile-menu').attr('class', 'animated slideOutUp');
 	});
 
 });
